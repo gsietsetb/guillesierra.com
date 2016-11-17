667a686c403397cb2a14ca86ffc745033894ad88
gsierra <glonchas@gmail.com>
2016-11-17 05:49:21 +0100

    v4.0 - Stable version - Added filter for project kind,  fix some small problems and drastic style change.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,115 +1,109 @@
-/*
-	Future Imperfect by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
-	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
-	});
+    skel.breakpoints({
+        xlarge:	'(max-width: 1680px)',
+        large:	'(max-width: 1280px)',
+        medium:	'(max-width: 980px)',
+        small:	'(max-width: 736px)',
+        xsmall:	'(max-width: 480px)'
+    });
 
-	$(function() {
+    $(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$menu = $('#menu'),
-			$sidebar = $('#sidebar'),
-			$main = $('#main');
+        var	$window = $(window),
+            $body = $('body'),
+            $menu = $('#menu'),
+            $sidebar = $('#sidebar'),
+            $main = $('#main');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+        $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+        $window.on('load', function() {
+            window.setTimeout(function() {
+                $body.removeClass('is-loading');
+            }, 100);
+        });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Fix: Placeholder polyfill.
+        $('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Prioritize "important" elements on medium.
+        skel.on('+medium -medium', function() {
+            $.prioritize(
+                '.important\\28 medium\\29',
+                skel.breakpoint('medium').active
+            );
+        });
 
-		// IE<=9: Reverse order of main and sidebar.
-			if (skel.vars.IEVersion <= 9)
-				$main.insertAfter($sidebar);
+        // IE<=9: Reverse order of main and sidebar.
+        if (skel.vars.IEVersion <= 9)
+            $main.insertAfter($sidebar);
 
-		// Menu.
-			$menu
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right',
-					target: $body,
-					visibleClass: 'is-menu-visible'
-				});
+        // Menu.
+        $menu
+            .appendTo($body)
+            .panel({
+                delay: 500,
+                hideOnClick: true,
+                hideOnSwipe: true,
+                resetScroll: true,
+                resetForms: true,
+                side: 'right',
+                target: $body,
+                visibleClass: 'is-menu-visible'
+            });
 
-		// Search (header).
-			var $search = $('#search'),
-				$search_input = $search.find('input');
+        // Search (header).
+        var $search = $('#search'),
+            $search_input = $search.find('input');
 
-			$body
-				.on('click', '[href="#search"]', function(event) {
+        $body
+            .on('click', '[href="#search"]', function(event) {
 
-					event.preventDefault();
+                event.preventDefault();
 
-					// Not visible?
-						if (!$search.hasClass('visible')) {
+                // Not visible?
+                if (!$search.hasClass('visible')) {
 
-							// Reset form.
-								$search[0].reset();
+                    // Reset form.
+                    $search[0].reset();
 
-							// Show.
-								$search.addClass('visible');
+                    // Show.
+                    $search.addClass('visible');
 
-							// Focus input.
-								$search_input.focus();
+                    // Focus input.
+                    $search_input.focus();
 
-						}
+                }
 
-				});
+            });
 
-			$search_input
-				.on('keydown', function(event) {
+        $search_input
+            .on('keydown', function(event) {
 
-					if (event.keyCode == 27)
-						$search_input.blur();
+                if (event.keyCode == 27)
+                    $search_input.blur();
 
-				})
-				.on('blur', function() {
-					window.setTimeout(function() {
-						$search.removeClass('visible');
-					}, 100);
-				});
+            })
+            .on('blur', function() {
+                window.setTimeout(function() {
+                    $search.removeClass('visible');
+                }, 100);
+            });
 
-		// Intro.
-			var $intro = $('#intro');
+        // Intro.
+        var $intro = $('#intro');
 
-			// Move to main on <=large, back to sidebar on >large.
-				skel
-					.on('+large', function() {
-						$intro.prependTo($main);
-					})
-					.on('-large', function() {
-						$intro.prependTo($sidebar);
-					});
+        // Move to main on <=large, back to sidebar on >large.
+        skel
+            .on('+large', function() {
+                $intro.prependTo($main);
+            })
+            .on('-large', function() {
+                $intro.prependTo($sidebar);
+            });
 
-	});
+    });
 
 })(jQuery);
