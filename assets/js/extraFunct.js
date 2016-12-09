c7a34672e6a27afd336201ec82cebd4efc14c3c2
gsierra <glonchas@gmail.com>
2016-12-09 14:40:47 -0300

    v5.0 - Launch version - Added dif pic, prototype and launch info, current release and countdown...

--- assets/js/extraFunct.js
+++ assets/js/extraFunct.js
@@ -1,3 +1,77 @@
+
+
+/** Toggle function*/
+$(".a").click(function() {
+    $(this).toggleClass('selected');
+
+    $(".courseBox").hide();
+
+    var datatypes = $(".courseBox");
+
+    $(".button.datatype").not('.selected').each(function() {
+        var selClass = $(this).attr('id').replace('Button', '');
+
+        datatypes = datatypes.filter(".courseBox[data-type!='" + selClass + "']");
+    });
+
+    $(".button.selected.datalocation").each(function() {
+        var selClass = $(this).attr('id').replace('Button', '');
+
+        datatypes.filter(".courseBox[data-location='" + selClass + "']").show();
+    });
+});
+
+
+var jobs=false, projects=false, studies=false, hack=false;
+
+var key = function(obj){
+  // some unique object-dependent key
+  return obj.totallyUniqueEmployeeIdKey; // just an example
+};
+
+function toggleDisplay(key){
+	window[key] = window[key] ? false : true;
+	projects ? toggleFilter("projects",true) : toggleFilter("projects",false);
+	jobs ? toggleFilter("jobs",true) : toggleFilter("jobs",false);
+	studies ? toggleFilter("studies",true) : toggleFilter("studies",false);
+	hack ? toggleFilter("hack",true) : toggleFilter("hack",false);
+}
+
+function toggleFilter(c,show) {
+    document.getElementById(c.concat("Filter")).style.opacity = show ? 1 : 0.5;
+	var elems = document.getElementsByClassName(c);
+	for(var j = 0; j != elems.length; ++j){
+		elems[j].style.display = show ? "inline" : "none";
+	}
+}
+
+/** NextImage function*/
+
+var i=1, b=1;
+function displayNextImageInsta() {
+	var src= "assets/img/imgRecog".concat(i).concat(".jpg");
+//					alert(src);
+	document.getElementById("imgSwitcher").src = src;
+	if(i++>=4){
+		i=1;
+	}
+}
+
+function displayNextImageCubecat() {
+	b == 1 ? b=3 : b=1;
+	// if(b==1)
+	// 	b=3;
+	// else
+	// 	b=1;
+	var src= "assets/img/nanosatlab_slideshow_0".concat(b).concat(".jpg");
+	document.getElementById("imgSwitcherCubecat").src = src;
+}
+
+function startTimer() {
+	setInterval(displayNextImageInsta, 4000);
+	setInterval(displayNextImageCubecat, 5000);
+}
+
 // /*
 //  Future Imperfect by HTML5 UP
 //  html5up.net | @ajlkn
@@ -3355,97 +3429,50 @@
 // 		return a.$ === m && (a.$ = fd), b && a.jQuery === m && (a.jQuery = ed), m
 // 	}, typeof b === K && (a.jQuery = a.$ = m), m
 // });
-
-/** Toggle function*/
-$(".a").click(function() {
-    $(this).toggleClass('selected');
-
-    $(".courseBox").hide();
-
-    var datatypes = $(".courseBox");
-
-    $(".button.datatype").not('.selected').each(function() {
-        var selClass = $(this).attr('id').replace('Button', '');
-
-        datatypes = datatypes.filter(".courseBox[data-type!='" + selClass + "']");
-    });
-
-    $(".button.selected.datalocation").each(function() {
-        var selClass = $(this).attr('id').replace('Button', '');
-
-        datatypes.filter(".courseBox[data-location='" + selClass + "']").show();
-    });
-});
-
-
-var jobs=false, projects=false, studies=false, hack=false;
-
-var key = function(obj){
-  // some unique object-dependent key
-  return obj.totallyUniqueEmployeeIdKey; // just an example
-};
-
-function toggleDisplay(key){
-	window[key] = window[key] ? false : true;
-	projects ? toggleFilter("projects",true) : toggleFilter("projects",false);
-	jobs ? toggleFilter("jobs",true) : toggleFilter("jobs",false);
-	studies ? toggleFilter("studies",true) : toggleFilter("studies",false);
-	hack ? toggleFilter("hack",true) : toggleFilter("hack",false);
+//Timer
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    // var miliseconds = Math.floor((t /1000));
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+        //, 'miliseconds': miliseconds
+    };
 }
 
-function toggleFilter(c,show) {
-    document.getElementById(c.concat("Filter")).style.opacity = show ? 1 : 0.5;
-	var elems = document.getElementsByClassName(c);
-	for(var j = 0; j != elems.length; ++j){
-		elems[j].style.display = show ? "inline" : "none";
-	}
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+    // var milisecondsSpan = clock.querySelector('.miliseconds');
+
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+        // milisecondsSpan.innerHTML = ('0' + t.miliseconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1);
 }
 
-/** NextImage function*/
-
-var i=1, b=1;
-function displayNextImageInsta() {
-	var src= "assets/img/imgRecog".concat(i).concat(".jpg");
-//					alert(src);
-	document.getElementById("imgSwitcher").src = src;
-	if(i++>=4){
-		i=1;
-	}
-}
-
-function displayNextImageCubecat() {
-	b == 1 ? b=3 : b=1;
-	// if(b==1)
-	// 	b=3;
-	// else
-	// 	b=1;
-	var src= "assets/img/nanosatlab_slideshow_0".concat(b).concat(".jpg");
-	document.getElementById("imgSwitcherCubecat").src = src;
-}
-
-function startTimer() {
-	setInterval(displayNextImageInsta, 4000);
-	setInterval(displayNextImageCubecat, 5000);
-}
-
-
-/** Maps*/
-
-<!--function initMap() {-->
-<!--var posNice = new google.maps.LatLng(43.7030414, 7.1828945);-->
-<!--var posMartorell = new google.maps.LatLng(41.4787706, 1.904357);-->
-<!--var posEPFL = new google.maps.LatLng(46.5190557, 6.5645689);-->
-<!--var posUpc = new google.maps.LatLng(43.7030414, 7.1828945);-->
-<!--var posGraz = new google.maps.LatLng(47.0735683, 15.3717506);-->
-<!--// Create a map object and specify the DOM element for display.-->
-<!--var mapOptions = {-->
-<!--zoom: 8, // initialize zoom level - the max value is 21-->
-<!--streetViewControl: false, // hide the yellow Street View pegman-->
-<!--scaleControl: true, // allow users to zoom the Google Map-->
-<!--mapTypeId: google.maps.MapTypeId.TERRAIN,-->
-<!--center: posNice,-->
-<!--tilt: 25-->
-<!--};-->
-<!--var map = new google.maps.Map(document.getElementById('map'),mapOptions);-->
-<!--map.set-->
-<!--}-->
+var deadline = new Date("January 1, 2017 09:00:00");//Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('clockdiv', deadline);
